6046004147dcbfcfa078ae49587bca13b2946647
Tomasz Nowak <tomasz89nowak@gmail.com>
2020-12-01 11:22:34 +0100

    Create utilityTypes.ts

---
+++ utilityTypes.ts
@@ -0,0 +1,10 @@
+/**
+ * Unwraps the result of function or promise
+ */
+export type Unwrap<T> = T extends Promise<infer U>
+  ? U
+  : T extends (...args: any) => Promise<infer U>
+  ? U
+  : T extends (...args: any) => infer U
+  ? U
+  : T;
